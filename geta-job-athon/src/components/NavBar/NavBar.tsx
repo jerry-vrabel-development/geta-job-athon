bcc03a4f447287900a8d3fdfe3658a7ae4c85d05
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2024-01-25 19:30:46 -0600

    TS config param

--- geta-job-athon/src/components/NavBar/NavBar.tsx
+++ geta-job-athon/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faHouse, faAddressCard, faEnvelope } from '@fortawesome/free-solid-svg-icons';
